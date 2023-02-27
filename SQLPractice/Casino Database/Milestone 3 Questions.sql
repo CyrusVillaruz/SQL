649d8c4d37296fa7355f3579450c3452faae3139
CyrusVillaruz <91701334+CyrusVillaruz@users.noreply.github.com>
2023-02-26 23:33:42 -0800

    Implemented 1/3 CasinoDB Tables

---
+++ SQLPractice/Casino Database/Milestone 3 Questions.sql
@@ -0,0 +1,16 @@
+# Number of hours each employee worked per week?
+# Number of labor hours last week?
+# List of which employees worked breaker shifts in the last month.
+# How many breaker shifts are scheduled this week?
+# How many Slot Attendants are scheduled today?
+# Any Slot Attendants who have not been assigned to Section NORTH in the last month
+# Number of active Written Warning (WW) employee A has?
+# List of active WW sorted by date and employee
+# A list of an employee's discipline/performance actions
+# Number of sick days an employee has available
+# Number of vacation days an employee has available
+# Statistics on Employees: Number of Female/Male employees, Average age of employees? Number of employees over 50? Under 30
+# List of employees who has the mandatory certification expiring in the next 6 weeks
+# List of employees who need updated in-house training
+# List of employees who have expired training
+# How many uniforms remain un-allocated?
